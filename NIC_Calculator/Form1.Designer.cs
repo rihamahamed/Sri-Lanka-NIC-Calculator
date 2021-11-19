c3cf18d7b8583dd52f8c8169f0f05c14e51bb03c
RIHAM AHAMED ABDUL RAHEEM <76843209+rihamfs@users.noreply.github.com>
2021-11-19 17:19:22 +0530

    NIC Calculator

---
+++ NIC_Calculator/Form1.Designer.cs
@@ -0,0 +1,238 @@
+﻿namespace NIC_Calculator
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.components = new System.ComponentModel.Container();
+            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(Form1));
+            this.label1 = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
+            this.label3 = new System.Windows.Forms.Label();
+            this.label4 = new System.Windows.Forms.Label();
+            this.txt_nic = new System.Windows.Forms.TextBox();
+            this.btn_check = new System.Windows.Forms.Button();
+            this.btn_clear = new System.Windows.Forms.Button();
+            this.label5 = new System.Windows.Forms.Label();
+            this.label6 = new System.Windows.Forms.Label();
+            this.textBox1 = new System.Windows.Forms.TextBox();
+            this.textBox2 = new System.Windows.Forms.TextBox();
+            this.textBox3 = new System.Windows.Forms.TextBox();
+            this.textBox4 = new System.Windows.Forms.TextBox();
+            this.timer1 = new System.Windows.Forms.Timer(this.components);
+            this.SuspendLayout();
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Font = new System.Drawing.Font("Times New Roman", 24F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label1.ForeColor = System.Drawing.Color.Transparent;
+            this.label1.Location = new System.Drawing.Point(27, 26);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(508, 36);
+            this.label1.TabIndex = 0;
+            this.label1.Text = "SRI LANKA NIC CALCULCATOR";
+            this.label1.Click += new System.EventHandler(this.label1_Click);
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.BackColor = System.Drawing.Color.Black;
+            this.label2.Font = new System.Drawing.Font("Hobo Std", 15.75F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label2.ForeColor = System.Drawing.Color.Transparent;
+            this.label2.Location = new System.Drawing.Point(40, 87);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(70, 30);
+            this.label2.TabIndex = 1;
+            this.label2.Text = "DATE";
+            this.label2.Click += new System.EventHandler(this.label2_Click);
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.BackColor = System.Drawing.Color.Black;
+            this.label3.Font = new System.Drawing.Font("Hobo Std", 15.75F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label3.ForeColor = System.Drawing.Color.Transparent;
+            this.label3.Location = new System.Drawing.Point(366, 87);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(66, 30);
+            this.label3.TabIndex = 1;
+            this.label3.Text = "TIME";
+            this.label3.Click += new System.EventHandler(this.label2_Click);
+            // 
+            // label4
+            // 
+            this.label4.AutoSize = true;
+            this.label4.Font = new System.Drawing.Font("Times New Roman", 15.75F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label4.ForeColor = System.Drawing.SystemColors.ControlText;
+            this.label4.Location = new System.Drawing.Point(54, 138);
+            this.label4.Name = "label4";
+            this.label4.Size = new System.Drawing.Size(149, 24);
+            this.label4.TabIndex = 2;
+            this.label4.Text = "Enter Your NIC";
+            // 
+            // txt_nic
+            // 
+            this.txt_nic.Font = new System.Drawing.Font("Times New Roman", 15.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.txt_nic.Location = new System.Drawing.Point(282, 130);
+            this.txt_nic.MaxLength = 12;
+            this.txt_nic.Name = "txt_nic";
+            this.txt_nic.Size = new System.Drawing.Size(235, 32);
+            this.txt_nic.TabIndex = 1;
+            // 
+            // btn_check
+            // 
+            this.btn_check.Font = new System.Drawing.Font("BritannicTBol", 18F, System.Drawing.FontStyle.Italic, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.btn_check.Location = new System.Drawing.Point(282, 184);
+            this.btn_check.Name = "btn_check";
+            this.btn_check.Size = new System.Drawing.Size(108, 42);
+            this.btn_check.TabIndex = 2;
+            this.btn_check.Text = "Check";
+            this.btn_check.UseVisualStyleBackColor = true;
+            this.btn_check.Click += new System.EventHandler(this.btn_check_Click);
+            // 
+            // btn_clear
+            // 
+            this.btn_clear.Font = new System.Drawing.Font("BritannicTBol", 18F, System.Drawing.FontStyle.Italic, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.btn_clear.Location = new System.Drawing.Point(409, 184);
+            this.btn_clear.Name = "btn_clear";
+            this.btn_clear.Size = new System.Drawing.Size(108, 42);
+            this.btn_clear.TabIndex = 3;
+            this.btn_clear.Text = "Clear";
+            this.btn_clear.UseVisualStyleBackColor = true;
+            this.btn_clear.Click += new System.EventHandler(this.button2_Click);
+            // 
+            // label5
+            // 
+            this.label5.AutoSize = true;
+            this.label5.Font = new System.Drawing.Font("Times New Roman", 14.25F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label5.Location = new System.Drawing.Point(357, 401);
+            this.label5.Name = "label5";
+            this.label5.Size = new System.Drawing.Size(187, 22);
+            this.label5.TabIndex = 7;
+            this.label5.Text = "RIHAM AHAME A.R";
+            // 
+            // label6
+            // 
+            this.label6.AutoSize = true;
+            this.label6.Font = new System.Drawing.Font("Times New Roman", 14.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label6.Location = new System.Drawing.Point(54, 239);
+            this.label6.Name = "label6";
+            this.label6.Size = new System.Drawing.Size(155, 147);
+            this.label6.TabIndex = 8;
+            this.label6.Text = "DATE of BIRTH  :\r\n\r\nGENDER             :\r\n\r\nAGE                    :\r\n\r\nCIVIL STA" +
+    "TUS    :";
+            // 
+            // textBox1
+            // 
+            this.textBox1.Font = new System.Drawing.Font("Times New Roman", 15.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.textBox1.Location = new System.Drawing.Point(214, 234);
+            this.textBox1.MaxLength = 12;
+            this.textBox1.Name = "textBox1";
+            this.textBox1.Size = new System.Drawing.Size(303, 32);
+            this.textBox1.TabIndex = 1;
+            // 
+            // textBox2
+            // 
+            this.textBox2.Font = new System.Drawing.Font("Times New Roman", 15.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.textBox2.Location = new System.Drawing.Point(214, 274);
+            this.textBox2.MaxLength = 12;
+            this.textBox2.Name = "textBox2";
+            this.textBox2.Size = new System.Drawing.Size(303, 32);
+            this.textBox2.TabIndex = 1;
+            // 
+            // textBox3
+            // 
+            this.textBox3.Font = new System.Drawing.Font("Times New Roman", 15.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.textBox3.Location = new System.Drawing.Point(214, 316);
+            this.textBox3.MaxLength = 12;
+            this.textBox3.Name = "textBox3";
+            this.textBox3.Size = new System.Drawing.Size(303, 32);
+            this.textBox3.TabIndex = 1;
+            // 
+            // textBox4
+            // 
+            this.textBox4.Font = new System.Drawing.Font("Times New Roman", 15.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.textBox4.Location = new System.Drawing.Point(214, 359);
+            this.textBox4.MaxLength = 12;
+            this.textBox4.Name = "textBox4";
+            this.textBox4.Size = new System.Drawing.Size(303, 32);
+            this.textBox4.TabIndex = 9;
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(9F, 19F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.BackColor = System.Drawing.Color.RoyalBlue;
+            this.ClientSize = new System.Drawing.Size(556, 431);
+            this.Controls.Add(this.textBox4);
+            this.Controls.Add(this.label6);
+            this.Controls.Add(this.label5);
+            this.Controls.Add(this.btn_clear);
+            this.Controls.Add(this.btn_check);
+            this.Controls.Add(this.textBox3);
+            this.Controls.Add(this.textBox2);
+            this.Controls.Add(this.textBox1);
+            this.Controls.Add(this.txt_nic);
+            this.Controls.Add(this.label4);
+            this.Controls.Add(this.label3);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.label1);
+            this.Font = new System.Drawing.Font("Times New Roman", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.Icon = ((System.Drawing.Icon)(resources.GetObject("$this.Icon")));
+            this.Margin = new System.Windows.Forms.Padding(4);
+            this.MaximizeBox = false;
+            this.MinimizeBox = false;
+            this.Name = "Form1";
+            this.ShowInTaskbar = false;
+            this.StartPosition = System.Windows.Forms.FormStartPosition.CenterScreen;
+            this.Text = "NIC CALCULCATOR";
+            this.Load += new System.EventHandler(this.Form1_Load);
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.Label label4;
+        private System.Windows.Forms.TextBox txt_nic;
+        private System.Windows.Forms.Button btn_check;
+        private System.Windows.Forms.Button btn_clear;
+        private System.Windows.Forms.Label label5;
+        private System.Windows.Forms.Label label6;
+        private System.Windows.Forms.TextBox textBox1;
+        private System.Windows.Forms.TextBox textBox2;
+        private System.Windows.Forms.TextBox textBox3;
+        private System.Windows.Forms.TextBox textBox4;
+        private System.Windows.Forms.Timer timer1;
+    }
+}
+
---
+++ NIC_Calculator/Form1.cs
@@ -0,0 +1,179 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace NIC_Calculator
+{
+    public partial class Form1 : Form
+    {
+
+        public Form1()
+        {
+            InitializeComponent();
+        }
+        
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+            label2.Text = DateTime.Now.ToLongDateString();
+            label3.Text = DateTime.Now.ToLongTimeString();
+        }
+
+        private void label2_Click(object sender, EventArgs e)
+        {
+        }
+
+        private void label5_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void btn_check_Click(object sender, EventArgs e)
+        {
+
+            int year, month = 1, day, age;
+            string gender = "MALE", nic,voter;
+            nic = txt_nic.Text;
+
+            if (nic.Length == 12)
+            {
+                year = int.Parse(nic.Substring(0, 4));
+                day = int.Parse(nic.Substring(4, 3));
+            }
+            else if (nic.Length == 10)
+            {
+                year = int.Parse("19" + nic.Substring(0, 2));
+                day = int.Parse(nic.Substring(2, 3));
+            }
+            else
+            {
+                string messege = "Plece Check Your NIC Number " + nic;
+                string title = "Invalid NIC Number";
+                MessageBoxButtons button = MessageBoxButtons.RetryCancel;
+                MessageBoxIcon icon = MessageBoxIcon.Error;
+                DialogResult result = MessageBox.Show(messege, title, button, icon);
+                if (result == DialogResult.Retry)
+                {
+                    txt_nic.Clear();
+                    txt_nic.Focus();
+                }
+                else
+                {
+                    this.Close();
+                }
+                return;
+            }
+
+            if (day > 500)
+            {
+                gender = "FEMALE";
+                day -= 500;
+            }
+
+            int[] Mday = { 31, 29, 31, 30, 31, 30, 31, 31, 30, 31, 30, 31 };
+
+            for (int i = 0; i < 12; i++)
+            {
+                if (day > Mday[i])
+                {
+                    month += 1;
+                    day -= Mday[i];
+                }
+            }
+
+            string smon = month.ToString();
+            if (month == 1)
+            {
+                smon = "JANUARY";
+            }
+            else if (month == 2)
+            {
+                smon = "FEBRUARY";
+            }
+            else if (month == 3)
+            {
+                smon = "MARCH";
+            }
+            else if (month == 4)
+            {
+                smon = "APRIL";
+            }
+            else if (month == 5)
+            {
+                smon = "MAY";
+            }
+            else if (month == 6)
+            {
+                smon = "JUNE";
+            }
+            else if (month == 7)
+            {
+                smon = "JULLY";
+            }
+            else if (month == 8)
+            {
+                smon = "AUGEST";
+            }
+            else if (month == 9)
+            {
+                smon = "SEPTEMBER";
+            }
+            else if (month == 10)
+            {
+                smon = "OCTOBER";
+            }
+            else if (month == 11)
+            {
+                smon = "NOVEMBER";
+            }
+            else if (month == 12)
+            {
+                smon = "DECEMBER";
+            }
+
+            //{"JANUARY", "FEB", "MARCH", "APRIL", "MAY", "JUNE", "JULLY", "AUGEST", "SEPTEMBER", "OCTOBER", "NOVEMBER", "DECEMBER" };
+
+            age = DateTime.Today.Year - year;
+
+
+            if (age > 18)
+            {
+                voter = "YOUR ELIGIBLE to VOTE";
+            }
+            else
+            {
+                int voteT = 18 - age;
+                string voteT1 = voteT.ToString();
+                voter = "YOUR NOT ELIGIBLE to VOTE WAIT FOR "+ voteT1;
+            }
+            string age1 = age.ToString();
+            textBox1.Text=(smon + " " + day + ", " + year);
+            textBox2.Text = (gender);
+            textBox3.Text = (age1);
+            textBox4.Text = (voter);
+
+
+
+        }
+        private void button2_Click(object sender, EventArgs e)
+        {
+            txt_nic.Clear();
+            textBox1.Clear();
+            textBox2.Clear();
+            textBox3.Clear();
+            textBox4.Clear();
+            txt_nic.Focus();
+        }
+
+        private void label1_Click(object sender, EventArgs e)
+        {
+
+        }
+    }
+ }
